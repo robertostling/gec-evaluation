7a296d037f953880a893f207628dfe516a166f62
Robert Östling <robert@ling.su.se>
2022-10-05 16:08:46 +0200

    Improved extraction script

--- baseline/extract_sprakbanken.py
+++ baseline/extract_sprakbanken.py
@@ -1,7 +1,8 @@
 """Script to extract plain text from Språkbanken XML data"""
 
 import sys
-import os.path
+import os
+import multiprocessing
 
 import bz2
 from xml.etree import ElementTree as ET
@@ -12,19 +13,31 @@ def extract_xml(xml_filename, outf):
         for event_type, element in context:
             if event_type == 'end' and element.tag == 'sentence':
                 tokens = [w.text for w in element.iter('w')]
-                if tokens:
+                if tokens and not (None in tokens):
                     print(' '.join(tokens), file=outf)
             if event_type == 'end' and element.tag in ('sentence', 'text'):
                 element.clear()
 
-def main():
-    out_filename = sys.argv[1]
-    xml_filenames = sys.argv[2:]
+def extract_wrapper(xml_filename, out_path):
+    name = os.path.basename(xml_filename).split('.')[0]
+    out_filename = os.path.join(out_path, f'{name}.txt')
+    temp_filename = os.path.join(out_path, f'{name}.txt.tmp')
+    if os.path.exists(out_filename):
+        print(f'Skipping {name}', flush=True)
+        return
+    with open(temp_filename, 'w', encoding='utf-8') as outf:
+        print(f'Extracting {name}...', flush=True)
+        extract_xml(xml_filename, outf)
+    os.rename(temp_filename, out_filename)
 
-    with open(out_filename, 'at', encoding='utf-8') as outf:
-        for xml_filename in xml_filenames:
-            print('Extracting', os.path.basename(xml_filename), flush=True)
-            extract_xml(xml_filename, outf)
+
+def main():
+    out_path = sys.argv[1]
+    xml_filenames = sys.argv[2:]
+    assert os.path.isdir(out_path)
+
+    with multiprocessing.Pool() as p:
+        p.starmap(extract_wrapper, [(fn, out_path) for fn in xml_filenames])
 
 if __name__ == '__main__':
     main()
