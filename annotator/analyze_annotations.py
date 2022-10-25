bfaa4c8ca5e4d651f14765be2a495590bac1b8be
Robert Östling <robert@ling.su.se>
2022-10-25 10:58:48 +0200

    Analysis script can now merge annotations from multiple annotators

--- annotator/analyze_annotations.py
+++ annotator/analyze_annotations.py
@@ -3,6 +3,8 @@ import json
 from collections import defaultdict, Counter
 import sys
 import statistics
+import copy
+
 import Levenshtein
 
 
@@ -14,25 +16,51 @@ class AnnotationResults:
                 self.annotations.append(json.load(f))
 
 
+    def merge_annotations(self, annotations):
+        merged = copy.deepcopy(annotations[0]['data'])
+        merged_sentences = [(example['original'], example['reference'])
+                            for example in merged]
+        for metadata in annotations[1:]:
+            data = metadata['data']
+            data_sentences = [(example['original'], example['reference'])
+                              for example in data]
+            if merged_sentences != data_sentences:
+                raise ValueError('Attempting to merge annotations of '
+                                 'different data')
+            for merged_example, data_example in zip(merged, data):
+                for system, merged_results in merged_example['systems'].items():
+                    if system in data_example['systems']:
+                        data_results = data_example['systems'][system]
+                        for annotator, annotations in \
+                                data_results['annotators'].items():
+                            if annotator in merged_results['annotators']:
+                                raise ValueError(
+                                        f'Annotator {annotator} present in '
+                                        'both annotation sets')
+                            merged_results['annotators'][annotator] = \
+                                    annotations
+
+        return merged
+
+
     def get_scores(self):
         system_measure_counts = defaultdict(lambda: defaultdict(Counter))
         system_metrics = defaultdict(lambda: defaultdict(list))
         features = ('Grammaticality', 'Fluency', 'Meaning Preservation')
-        for metadata in self.annotations:
-            data = metadata['data']
-            for example in data:
-                for system, results in example['systems'].items():
-                    for annotator, annotations in results['annotators'].items():
-                        for feature in features:
-                            if feature in annotations:
-                                n = annotations[feature]
-                                system_measure_counts[system][feature][n] += 1
-                        if 'corrected' in annotations:
-                            s1 = results['output']
-                            s2 = annotations['corrected']
-                            ld = Levenshtein.distance(s1, s2)
-                            nld = ld / max(len(s1), len(s2))
-                            system_metrics[system]['NLD'].append(nld)
+        data = self.merge_annotations(self.annotations)
+        for example in data:
+            for system, results in example['systems'].items():
+                for annotator, annotations in results['annotators'].items():
+                    for feature in features:
+                        if feature in annotations:
+                            n = annotations[feature]
+                            system_measure_counts[system][feature][n] += 1
+                    if 'corrected' in annotations:
+                        s1 = results['output']
+                        s2 = annotations['corrected']
+                        ld = Levenshtein.distance(s1, s2)
+                        nld = ld / max(len(s1), len(s2))
+                        system_metrics[system]['NLD'].append(nld)
         return system_measure_counts, system_metrics
 
 
